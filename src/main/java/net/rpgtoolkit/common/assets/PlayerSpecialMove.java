7dc648a364ca56c2161b165b48b0faa88732f7ac
Joshua Michael Daly <b00056835@student.itb.ie>
2015-11-30 19:46:16 +0000

    Added missing setters to PlayerSpecialMove to make it compatible with the Character editor.
    Signed-off-by: Joshua Michael Daly <b00056835@student.itb.ie>

--- src/main/java/net/rpgtoolkit/common/assets/PlayerSpecialMove.java
+++ src/main/java/net/rpgtoolkit/common/assets/PlayerSpecialMove.java
@@ -1,56 +1,70 @@
 /**
  * Copyright (c) 2015, rpgtoolkit.net <help@rpgtoolkit.net>
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of
+ * the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package net.rpgtoolkit.common.assets;
 
 /**
- * 
+ *
  * @author Geoff Wilson
  * @author Joshua Michael Daly
  */
-public class PlayerSpecialMove
-{
-    private String name;
-    private long minExperience;
-    private long minLevel;
-    private String conditionVariable;
-    private String conditionVariableTest;
+public class PlayerSpecialMove {
 
-    public PlayerSpecialMove(String name, long minExperience, long minLevel, String cVar, String cVarTest)
-    {
-        this.name = name;
-        this.minExperience = minExperience;
-        this.minLevel = minLevel;
-        this.conditionVariable = cVar;
-        this.conditionVariableTest = cVarTest;
-    }
+  private String name;
+  private long minExperience;
+  private long minLevel;
+  private String conditionVariable;
+  private String conditionVariableTest;
 
-    public String getName()
-    {
-        return this.name;
-    }
+  public PlayerSpecialMove(String name, long minExperience, long minLevel, String cVar, String cVarTest) {
+    this.name = name;
+    this.minExperience = minExperience;
+    this.minLevel = minLevel;
+    this.conditionVariable = cVar;
+    this.conditionVariableTest = cVarTest;
+  }
 
-    public long getMinExperience()
-    {
-        return this.minExperience;
-    }
+  public String getName() {
+    return this.name;
+  }
 
-    public long getMinLevel()
-    {
-        return this.minLevel;
-    }
+  public long getMinExperience() {
+    return this.minExperience;
+  }
 
-    public String getConditionVariable()
-    {
-        return this.conditionVariable;
-    }
+  public long getMinLevel() {
+    return this.minLevel;
+  }
 
-    public String getConditionVariableTest()
-    {
-        return this.conditionVariableTest;
-    }
+  public String getConditionVariable() {
+    return this.conditionVariable;
+  }
+
+  public String getConditionVariableTest() {
+    return this.conditionVariableTest;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setMinExperience(long minExperience) {
+    this.minExperience = minExperience;
+  }
+
+  public void setMinLevel(long minLevel) {
+    this.minLevel = minLevel;
+  }
+
+  public void setConditionVariable(String conditionVariable) {
+    this.conditionVariable = conditionVariable;
+  }
+
+  public void setConditionVariableTest(String conditionVariableTest) {
+    this.conditionVariableTest = conditionVariableTest;
+  }
+
 }
