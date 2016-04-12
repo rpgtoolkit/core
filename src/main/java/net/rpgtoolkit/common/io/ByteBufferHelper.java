ea3a07269c33fedb7f95d897ed4732ed86e29a17
Chris Hutchinson <chris@cshutchinson.com>
2016-04-11 21:45:21 -0400

    Implemented ByteBufferHelper#putTerminatedString to assist writing null-terminated strings to byte buffers

--- src/main/java/net/rpgtoolkit/common/io/ByteBufferHelper.java
+++ src/main/java/net/rpgtoolkit/common/io/ByteBufferHelper.java
@@ -7,6 +7,7 @@ import java.nio.charset.Charset;
  * @author Chris Hutchinson <chris@cshutchinson.com>
  */
 public final class ByteBufferHelper {
+
   public static final Charset LEGACY_CHARSET = Charset.forName("US-ASCII");
 
   public static String getTerminatedString(final ByteBuffer buffer) {
@@ -22,14 +23,39 @@ public final class ByteBufferHelper {
   }
 
   /**
-   * Converts a String to bytes using the legacy character set and returns a new buffer containing
-   * the String's bytes, null-terminated, with its position at 0, ready to write.
+   * Writes a null-terminated string into a byte buffer with the specified encoding.
    *
-   * @param s a String to create a null-terminated buffer for
+   * @param buffer
+   *   buffer to write string into
+   * @param str
+   *   string to write into the buffer, if null a single null byte is written
+   * @param charset
+   *   character set to use as encoding, if null the platform character set is used.
+   */
+  public static void putTerminatedString(final ByteBuffer buffer, String str, Charset charset) {
+    if (buffer == null)
+      throw new NullPointerException();
+    if (charset == null)
+      charset = Charset.defaultCharset();
+    if (buffer.remaining() < 1)
+      throw new IllegalStateException("buffer is full");
+    if (str != null) {
+      final byte[] encoded = str.getBytes(charset);
+      if (buffer.remaining() < encoded.length + 1)
+        throw new IllegalStateException("string length exceeds remaining buffer length");
+      buffer.put(encoded);
+    }
+    buffer.put((byte) 0);
+  }
+
+  /**
+   * Converts a String to bytes using the legacy character set and returns a new buffer containing
+   * the String's bytes, with its position at 0, ready to write.
+   *
+   * @param s a String to create a buffer for
    * @return a new buffer with the String's bytes
    */
   public static ByteBuffer getBuffer(String s) {
-    s += '\0';
     return ByteBuffer.wrap(s.getBytes(LEGACY_CHARSET));
   }
 
